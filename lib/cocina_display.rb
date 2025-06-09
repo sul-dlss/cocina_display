1e4930e7ca8809747a12c40bee76ad388b52d0ef
Nick Budak <budak@stanford.edu>
2025-06-09 13:10:07 -0700

    Initial commit

---
+++ lib/cocina_display.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cocina_display/version"
+
+module CocinaDisplay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cocina_display_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CocinaDisplay do
+  it "has a version number" do
+    expect(CocinaDisplay::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
