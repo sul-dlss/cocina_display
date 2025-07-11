0054760ac1e98dba9ea3353537cc146320218e3c
Nick Budak <budak@stanford.edu>
2025-07-11 14:11:20 -0700

    Add subject faceting aggregation methods
    Note – no geographic subject yet; planning to do that with other
    geo-related methods in a separate PR. They will get added to
    subject_all_facet when finished.

--- lib/cocina_display/concerns/subjects.rb
+++ lib/cocina_display/concerns/subjects.rb
@@ -42,6 +42,39 @@ module CocinaDisplay
         subjects.filter { |s| s.is_a? NameSubject }.map(&:display_str).uniq
       end
 
+      # Combination of all subject values for faceting.
+      # @see #subject_facet
+      # @see #subject_temporal_genre_facet
+      # @return [Array<String>]
+      def subject_all_facet
+        subject_facet + subject_temporal_genre_facet
+      end
+
+      # Combination of topic, occupation, name, and title subject values for faceting.
+      # @see #subject_topics
+      # @see #subject_other_facet
+      # @return [Array<String>]
+      def subject_facet
+        subject_topics + subject_other_facet
+      end
+
+      # Combination of occupation, name, and title subject values for faceting.
+      # @see #subject_occupations
+      # @see #subject_names
+      # @see #subject_titles
+      # @return [Array<String>]
+      def subject_other_facet
+        subject_occupations + subject_names + subject_titles
+      end
+
+      # Combination of temporal and genre subject values for faceting.
+      # @see #subject_temporal
+      # @see #subject_genres
+      # @return [Array<String>]
+      def subject_temporal_genre_facet
+        subject_temporal + subject_genres
+      end
+
       private
 
       # All subjects, accessible as Subject objects.
