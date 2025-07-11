b6066e67bad7d3d6f515403bda0f0863f1494400
Nick Budak <budak@stanford.edu>
2025-07-11 10:46:07 -0700

    Support querying integer range of pub years
    Closes #28

--- spec/concerns/events_spec.rb
+++ spec/concerns/events_spec.rb
@@ -243,6 +243,68 @@ RSpec.describe CocinaDisplay::CocinaRecord do
     end
   end
 
+  describe "#pub_year_int_range" do
+    subject { record.pub_year_int_range }
+
+    context "with a valid date range (2020-01-01 to 2021-10-31)" do
+      let(:dates) do
+        [
+          {
+            "structuredValue" => [
+              {"value" => "2020-01-01", "type" => "start", "encoding" => {"code" => "w3cdtf"}},
+              {"value" => "2021-10-31", "type" => "end", "encoding" => {"code" => "w3cdtf"}}
+            ],
+            "type" => "publication"
+          }
+        ]
+      end
+
+      it { is_expected.to eq([2020, 2021]) }
+    end
+
+    context "with an EDTF interval (6 BCE to 5 BCE)" do
+      let(:dates) do
+        [
+          {
+            "value" => "-0005/-0004", "type" => "publication", "encoding" => {"code" => "edtf"}
+          }
+        ]
+      end
+
+      it { is_expected.to eq([-5, -4]) }
+    end
+
+    context "with a single year date (2020)" do
+      let(:dates) do
+        [
+          {"value" => "2020", "type" => "publication"}
+        ]
+      end
+
+      it { is_expected.to eq([2020]) }
+    end
+
+    context "with a decade date (209x)" do
+      let(:dates) do
+        [
+          {"value" => "209x", "type" => "publication", "encoding" => {"code" => "edtf"}}
+        ]
+      end
+
+      it { is_expected.to eq([2090, 2091, 2092, 2093, 2094, 2095, 2096, 2097, 2098, 2099]) }
+    end
+
+    context "with an invalid date" do
+      let(:dates) do
+        [
+          {"value" => "invalid-date", "type" => "publication"}
+        ]
+      end
+
+      it { is_expected.to be_nil }
+    end
+  end
+
   describe "#imprint_display_str" do
     subject { record.imprint_display_str }
 
