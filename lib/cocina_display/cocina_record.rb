56fee7a95f3f2f483a5866b9475e3379d502d48e
Justin Coyne <jcoyne@justincoyne.com>
2025-08-29 14:19:16 -0500

    use the Cocina namespace for extracting content-type

--- lib/cocina_display/cocina_record.rb
+++ lib/cocina_display/cocina_record.rb
@@ -76,7 +76,7 @@ module CocinaDisplay
     # @example
     #  record.content_type #=> "image"
     def content_type
-      cocina_doc["type"]&.split("/")&.last
+      cocina_doc["type"].delete_prefix("https://cocina.sul.stanford.edu/models/")
     end
 
     # Primary processing label for the object.
