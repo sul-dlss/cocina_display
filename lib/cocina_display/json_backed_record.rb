f3f60e8d4aae2af5565b3515b11e895ebf5c1f8b
Nick Budak <budak@stanford.edu>
2026-01-07 12:02:30 -0800

    Add a method for retrieving the entire record text
    Closes #225

--- lib/cocina_display/json_backed_record.rb
+++ lib/cocina_display/json_backed_record.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 module CocinaDisplay
+  # Superclass for metadata records backed by a Cocina JSON document.
   class JsonBackedRecord
     # The parsed Cocina document.
     # @return [Hash]
     attr_reader :cocina_doc
 
-    # Initialize a CocinaRecord with a Cocina document hash.
+    # Initialize a record with a Cocina document hash.
     # @param cocina_doc [Hash]
     def initialize(cocina_doc)
       @cocina_doc = cocina_doc
@@ -22,6 +23,23 @@ module CocinaDisplay
     #  record.path("$.description.contributor[?(@.type == 'person')].name.*.value").search #=> ["Smith, John"]
     def path(path_expression)
       Janeway.enum_for(path_expression, cocina_doc)
+    end
+
+    # Flattened, normalized aggregation of all node texts in the Cocina document.
+    # @note Used for 'all search' fields in indexing.
+    # @return [String]
+    def text
+      node_texts.compact.join(" ").gsub(/\s+/, " ").strip
+    end
+
+    private
+
+    # Array of all node values/codes except those under "source" keys.
+    # Used to build flattened text representation.
+    # @note Source values are omitted because they usually indicate ontologies/vocabularies.
+    # @return [Array<String>]
+    def node_texts
+      path("$..['code', 'value']").map { |node, _p, _i, path| node unless path.to_s.include?("['source']") }
     end
   end
 end
