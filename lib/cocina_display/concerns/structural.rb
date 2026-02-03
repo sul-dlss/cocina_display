b759602e554bcb593867da0cba785a0c1bf67a6f
Nick Budak <budak@stanford.edu>
2026-02-03 14:36:44 -0800

    Support checking the unique fileset types
    Used in Searchworks indexing.

--- lib/cocina_display/concerns/structural.rb
+++ lib/cocina_display/concerns/structural.rb
@@ -16,11 +16,32 @@ module CocinaDisplay
         @files ||= path("$.structural.contains.*.structural.contains.*").search
       end
 
+      # Structured data for all file sets in the object.
+      # Each fileset contains one or more files.
+      # @return [Array<Hash>]
+      # @example
+      #  record.filesets.each do |fileset|
+      #   puts fileset["type"] #=> "image"
+      #   puts fileset["label"] #=> "High Resolution Images"
+      #  end
+      def filesets
+        @filesets ||= path("$.structural.contains.*").search
+      end
+
       # All unique MIME types of files in this object.
       # @return [Array<String>]
       # @example ["image/jpeg", "application/pdf"]
       def file_mime_types
         files.pluck("hasMimeType").uniq
+      end
+
+      # All unique types of filesets in this object.
+      # @return [Array<String>]
+      # @example ["image", "document"]
+      def fileset_types
+        filesets.pluck("type")
+          .map { |type| type.delete_prefix("https://cocina.sul.stanford.edu/models/resources/") }
+          .uniq
       end
 
       # Human-readable string representation of {total_file_size_int}.
@@ -44,12 +65,17 @@ module CocinaDisplay
         path("$.structural.isMemberOf.*").map { |druid| druid.delete_prefix("druid:") }
       end
 
+      # Whether this object is a virtual object.
+      # @return [Boolean]
       def virtual_object?
-        return false if path("$.structural.contains.*").any?
+        return false if filesets.any?
 
         path("$.structural.hasMemberOrders.*.members.*").any?
       end
 
+      # DRUIDs of members of this virtual object.
+      # @return [Array<String>]
+      # @example ["ts786ny5936", "tp006ms8736", "tj297ys4758"]
       def virtual_object_members
         return [] unless virtual_object?
 
